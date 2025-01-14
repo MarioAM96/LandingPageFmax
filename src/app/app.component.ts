afa27098621117ada87c7bd9aac0af6fb1ae7721
Mario Muñoz <mario.munoz@fibramax.ec>
2025-01-14 10:42:21 -0500

    header and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'LandingPageFmax';
 }
+
